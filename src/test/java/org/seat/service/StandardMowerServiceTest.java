de5b7a20890130dfad89c8d9f6e3ef4017d58d80
Guillermo Fernández <guillermo_fernandez_raboso@carrefour.com>
2022-10-25 14:41:09 +0200

    test - minor changes

--- src/test/java/org/seat/service/StandardMowerServiceTest.java
+++ src/test/java/org/seat/service/StandardMowerServiceTest.java
@@ -1,9 +1,6 @@
 package org.seat.service;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.seat.domain.exceptions.CustomException;
 import org.seat.domain.service.IMowerService;
 import org.seat.domain.service.StandardMowerService;
@@ -20,18 +17,13 @@ class StandardMowerServiceTest {
     private final PrintStream standardOut = System.out;
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
-    List<String> inputOK = List.of("5 5", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM");
-    List<String> inputWithout5LinesNotOK = List.of("3 4", "1 4 S");
-    List<String> inputFirstMowerOutOfBoundariesNotOK = List.of("5 5", "6 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM");
-    List<String> inputIncorrectMovementsNotOK = List.of("5 5", "6 2 N", "ZXY", "3 3 E", "AAA");
-
     @BeforeEach
     void setUp() {
         System.setOut(new PrintStream(outputStreamCaptor));
     }
 
     @AfterEach
-    public void tearDown() {
+    void tearDown() {
         System.setOut(standardOut);
     }
 
@@ -39,10 +31,14 @@ class StandardMowerServiceTest {
     @DisplayName("Should return the specific result for the provided movements")
     void executeMovements_dataOK_expectResultOK() throws CustomException {
 
-        IMowerService mowerService = new StandardMowerService(inputOK);
+        // given
+        List<String> inputOK = List.of("5 5", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM");
 
+        // when
+        IMowerService mowerService = new StandardMowerService(inputOK);
         mowerService.executeMovements();
 
+        // then
         assertThat(outputStreamCaptor.toString().trim()).contains("1 3 N");
         assertThat(outputStreamCaptor.toString().trim()).contains("5 1 E");
     }
@@ -51,13 +47,18 @@ class StandardMowerServiceTest {
     @DisplayName("Should return exception due to insufficient number input data lines")
     void executeMovements_dataWithout5LinesNotOK_expectException() {
 
+        // given
+        List<String> inputWithout5LinesNotOK = List.of("3 4", "1 4 S");
+        String expectedMessage = "only 5 lines are needed";
+
+        // when
         CustomException exception = assertThrows(CustomException.class, () ->
             new StandardMowerService(inputWithout5LinesNotOK).executeMovements()
         );
 
-        String expectedMessage = "only 5 lines are needed";
         String actualMessage = exception.getFault().getResultDescription();
 
+        // then
         assertTrue(actualMessage.contains(expectedMessage));
     }
 
@@ -65,13 +66,18 @@ class StandardMowerServiceTest {
     @DisplayName("Should return exception due to mower out of boundaries")
     void executeMovements_firstMowerOutOfBoundariesNotOK_expectException() {
 
+        // given
+        List<String> inputFirstMowerOutOfBoundariesNotOK = List.of("5 5", "6 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM");
+        String expectedMessage = "Initial position of the mower number 1 is outside the plateau";
+
+        // when
         CustomException exception = assertThrows(CustomException.class, () ->
                 new StandardMowerService(inputFirstMowerOutOfBoundariesNotOK).executeMovements()
         );
 
-        String expectedMessage = "Initial position of the mower number 1 is outside the plateau";
         String actualMessage = exception.getFault().getResultDescription();
 
+        // then
         assertEquals(expectedMessage, actualMessage);
     }
 
@@ -79,13 +85,18 @@ class StandardMowerServiceTest {
     @DisplayName("Should return exception due incorrect movements informed")
     void executeMovements_incorrectMovementsNotOK_expectException() {
 
+        // given
+        List<String> inputIncorrectMovementsNotOK = List.of("5 5", "6 2 N", "ZXY", "3 3 E", "AAA");
+        String expectedMessage = "line 3 and 5 can only have the values “L” for left turn, “R” for right turn and ”M” for movement";
+
+        // when
         CustomException exception = assertThrows(CustomException.class, () ->
                 new StandardMowerService(inputIncorrectMovementsNotOK).executeMovements()
         );
 
-        String expectedMessage = "line 3 and 5 can only have the values “L” for left turn, “R” for right turn and ”M” for movement";
         String actualMessage = exception.getFault().getResultDescription();
 
+        // then
         assertTrue(actualMessage.contains(expectedMessage));
     }
 }
